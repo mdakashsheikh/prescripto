38437593bd4340f0122fc0895549c23e3c01d5f3
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-10-02 23:58:07 +0600

    node

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,7 +3,9 @@ import { assets } from '../assets/assets_frontend/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+
     const navigate = useNavigate()
+    
     return (
         <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
             {/* ------- LEFT SIDE ------- */}
