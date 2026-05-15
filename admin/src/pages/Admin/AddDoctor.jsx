3cc942d64595caeea2be59a324fad94ebdaab32a
MdAkashSheikh <nmakashsheikh@gmail.com>
2026-05-15 23:58:08 +0600

    fixed code quality

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -73,7 +73,7 @@ const AddDoctor = () => {
         <form onSubmit={onSubmitHandler} className='m-5 w-full'>
             <p className='mb-3 text-lg font-medium'>Add Doctor</p>
 
-            <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll'>
+            <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[85vh] overflow-y-scroll'>
                 <div className='flex items-center gap-4 mb-8 text-gray-500'>
                     <label htmlFor='doc-img'>
                         <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={docImg ? URL.createObjectURL(docImg) : assets.upload_area} alt=''/>
